a30934d8ec0d0a110a74f9e7d65e55bdc34df80b
sksay <sksay2486@naver.com>
2021-12-09 23:09:34 +0900

    Update .gitignore, README.md, and 25 more files...

--- nodeServer/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-app.use(express.static(path.resolve(__dirname)));
-
-http.createServer(app).listen(8081);
